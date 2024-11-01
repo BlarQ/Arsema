9249a88fdc725392fed8c1ef44ccd98ff7435a2c
BlarQ <collinsogunlala@gmail.com>
2024-11-01 17:19:59 +0100

    feat: fixing footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const footLinks = [
+    { link: 'Home', url: '/' },
+    { link: 'Contact Us', url: '/contact' },
+    { link: 'Privacy Policy', url: '/privacy' },
+    { link: 'Terms & Conditions', url: '/terms' },
+    { link: 'Refund Policy', url: '/help' },
+    { link: 'Shipping Policy', url: '/help' },
+    { link: 'FAQs', url: '/social' }
+
+]
+
+const Footer = () => {
+  return (
+    <footer className='bg-[#f2efea] flex flex-col gap-4 divide-y px-[5%] text-[#4a6b92] bottom-0 fixed inset-x-0'>
+        <nav className='pt-4'>
+            <ul className='flex items-center justify-center gap-8'>
+                {footLinks.map(link => (
+                    <li key={link.link} className='hover:text-[gray] duration-300'>
+                        <a href={link.url}>{link.link}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+
+        <div className='flex items-center justify-center py-4'>
+            <p>&copy; {new Date().getFullYear()}. ARESMA PURE NATURALS - Desined by Pholoba</p>
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Header from "./components/Header";
 import BottomHeader from "./components/BottomHeader";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Footer />
     </div>
   );
 }
