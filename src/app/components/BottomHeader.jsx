116b5c6d1e14123cfcefd231d01e347ceaeebdf6
BlarQ <collinsogunlala@gmail.com>
2024-11-03 02:57:22 +0100

    feat: login deactivated

--- src/app/components/BottomHeader.jsx
+++ src/app/components/BottomHeader.jsx
@@ -17,9 +17,9 @@ const BottomHeader = () => {
     return (
         <div className='flex flex-wrap items-center justify-center sm:justify-end px-[5%] py-1 bg-[#f2efea]'>
             <ul className='flex flex-wrap items-center justify-center md:justify-end gap-6 md:gap-10 text-[#4a6b92]'>
-                <li className='hover:text-[gray] duration-300'>
+                {/* <li className='hover:text-[gray] duration-300'>
                     <Link href={'/login'}>Log in</Link>
-                </li>
+                </li> */}
                 <li className='hover:text-[gray] duration-300 flex items-center'>
                     <IoMdCart />
                     <Link href={'/cart'}>Cart ({cartCount})</Link> {/* Use cartCount state */}
