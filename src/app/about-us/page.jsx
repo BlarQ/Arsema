d0e2946d5ae9c5632e9d43fcd66a08d34e7d6893
BlarQ <collinsogunlala@gmail.com>
2024-11-04 18:14:47 +0100

    feat: added about

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+
+const page = () => {
+  return (
+    <div>
+      <Hero />
+      about us
+
+      <Footer />
+    </div>
+  )
+}
+
+export default page
